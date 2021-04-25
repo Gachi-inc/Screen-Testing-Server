4e1cabbe9b42df3150f77d6c39986af98cdf5e6a
Vladimir <43116496+VladimirSTAR0911@users.noreply.github.com>
2021-04-25 08:59:38 +0500

    server sqLite

--- server/server-init-socketio.py
+++ server/server-init-socketio.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, jsonify, send_from_directory
 from flask_socketio import SocketIO, emit, join_room
 from flask_cors import CORS
+from sqlLite.BdInit import DataBase 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'development key'
@@ -8,29 +9,26 @@ socket = SocketIO(
     app, cors_allowed_origins="*", engineio_logger=True, asunc_mode='eventlet'
 )
 CORS(app)
+db = DataBase("sites.db")
 
 
-@app.route('/api/screenshots', methods=['GET', 'POST'])
-def hello_world():
+@app.route('/api/screenshots', methods=['POST'])
+def get_analise():
+    request_data = request.get_json()
+    # db.addSiteData(request_data)
+    # socket.emit({"result" : "Change has been made"})
+    return "OK"
+
+
+@app.route('/api/testing', methods=['POST'])
+def trace():
     request_data = request.get_json()
     url = request_data['url']
-    return ""
-
-
-# @app.route('/screenshots', methods=['GET', 'POST'])
-# def parse_request():
-#     if request.method == 'POST':
-#         first = request.form.get('first')
-#         second = request.form.get('second')
-#         return '''
-#                     <h1>The language value is: {}</h1>
-#                     <h1>The framework value is: {}</h1>'''.format(first, second)
-#     return '''
-#            <form method="POST">
-#                <div><label>Language: <input type="text" name="language"></label></div>
-#                <div><label>Framework: <input type="text" name="framework"></label></div>
-#                <input type="submit" value="Submit">
-#            </form>'''
+    # result = db.addURLData(request_data)
+    # Обработка
+    #  socket.emit('List3', data)
+    #print(result)
+    return "ss"
 
 @socket.on('message')
 def handle_message(data):
@@ -39,12 +37,12 @@ def handle_message(data):
     socket.emit('List3', data)
 
 
-@socket.on('List2')
-def colors(msg):
-    print(msg)
-    socket.send(msg)
-    socket.emit('List3', msg)
+# @socket.on('List2')
+# def colors(msg):
+#     print(msg)
+#     socket.send(msg)
+#     socket.emit('List3', msg)
 
 
 if __name__ == '__main__':
-    socket.run(app, host='192.168.1.7', port='6543', debug=True)
+    socket.run(app, host='192.168.1.202', port='6543', debug=True)
